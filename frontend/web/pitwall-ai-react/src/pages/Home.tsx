3d89660a7dc7f41f3372bb7c5f5735dd2f52a21b
DuttatreyAmitesh Sethi <amiteshsethi@gmail.com>
2026-03-17 13:15:01 +0530

    ( ui ) fix : casing issue for frontend deployement

--- frontend/web/pitwall-ai-react/src/pages/Home.tsx
+++ frontend/web/pitwall-ai-react/src/pages/Home.tsx
@@ -7,7 +7,7 @@ import {
 } from "../api/pitwall";
 import type { UpcomingRace, PredictionComparison } from "../types";
 import F1Loader from "../components/F1loader";
-import { useCountDownFn } from "../hooks/useCountDown";
+import { useCountdown } from "../hooks/customhooks";
 
 export default function Home() {
   const [race, setRace] = useState<UpcomingRace | null>(null);
@@ -18,7 +18,7 @@ export default function Home() {
     null,
   );
 
-  const timeLeft = useCountDownFn(race?.date ?? null, race?.time ?? null);
+  const timeLeft = useCountdown(race?.date ?? null, race?.time ?? null);
 
   useEffect(() => {
     Promise.all([getUpcomingRace(), getPredictionComparison()])
